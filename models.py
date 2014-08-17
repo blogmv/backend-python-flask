638bcb849df5401cfe06b658902acd01679c9a66
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-08-16 21:43:05 -0300

    Updated CORS, added date field to the models and added order option in the API

--- models.py
+++ models.py
@@ -1,35 +1,38 @@
 from google.appengine.ext import ndb
 from json_serializer import JSONSerializable
 
+
 class Article(ndb.Model, JSONSerializable):
-	title = ndb.StringProperty(indexed=False)
-	content = ndb.StringProperty(indexed=False)
+    title = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
-	@property
-	def id(self):
-		return self.key.integer_id()
+    @property
+    def id(self):
+        return self.key.integer_id()
 
-	def toJSON(self):
-		return {
-			"id": self.id,
-			"title": self.title,
-			"content": self.content
-		}
+    def toJSON(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content
+        }
 
 class Comment(ndb.Model, JSONSerializable):
-	article = ndb.KeyProperty(kind=Article, indexed=True)
-	author_name = ndb.StringProperty(indexed=False)
-	author_email = ndb.StringProperty(indexed=False)
-	content = ndb.StringProperty(indexed=False)
+    article = ndb.KeyProperty(kind=Article, indexed=True)
+    author_name = ndb.StringProperty(indexed=False)
+    author_email = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
-	@property
-	def id(self):
-		return self.key.integer_id()
+    @property
+    def id(self):
+        return self.key.integer_id()
 
-	def toJSON(self):
-		return {
-			"id": self.id,
-			"author_name": self.author_name,
-			"author_email": self.author_email,
-			"content": self.content
-		}
+    def toJSON(self):
+        return {
+            "id": self.id,
+            "author_name": self.author_name,
+            "author_email": self.author_email,
+            "content": self.content
+        }
