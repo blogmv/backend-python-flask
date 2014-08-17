fc4084fdf04cbf8aa0df532ac3685b39d0b67cca
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-08-16 21:47:44 -0300

    Reformat the code

--- json_serializer.py
+++ json_serializer.py
@@ -2,9 +2,11 @@ import json
 
 __author__ = 'fernando'
 
+
 class JSONSerializable(object):
     def toJSON(self):
         raise NotImplementedError("Implement JSONSerializable.toJSON")
+
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
